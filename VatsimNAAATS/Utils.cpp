2754255d3b481575920284aa7452f1e1206f5cb4
Andrew Ogden <andrewogden@bigpond.com>
2020-09-23 10:11:30 +1000

    sneaky utils refactor

--- VatsimNAAATS/Utils.cpp
+++ VatsimNAAATS/Utils.cpp
@@ -69,3 +69,81 @@ void Utils::LoadPluginData(CPlugIn* plugin) {
 	// Show a user message saying that the plugin was loaded successfully
 	plugin->DisplayUserMessage("Message", "vNAAATS Plugin", string("version " + PLUGIN_VERSION + " loaded successfully.").c_str(), false, false, false, false, false);
 }
+
+string Utils::ParseZuluTime(bool delimit, CFlightPlan* fp = nullptr, int ep = -1) {
+	time_t now = time(0);
+	tm* zuluTime = gmtime(&now);
+	int deltaMinutes = 0;
+	if (ep != -1) {
+		deltaMinutes = fp->GetExtractedRoute().GetPointDistanceInMinutes(ep);
+	}
+	int hours = zuluTime->tm_hour;
+	int minutes = zuluTime->tm_min + deltaMinutes;
+
+	if (minutes >= 60) {
+		// Get minutes
+		int minRemainder = minutes % 60;
+
+		// Get number of hours
+		hours += (minutes - minRemainder) / 60;
+
+		// Reassign number of minutes
+		minutes = minRemainder;
+	}
+
+	// Check if over 24 hours
+	if (hours >= 24) {
+		hours = hours - 24;
+	}
+
+
+	// Pad for zeros
+	string strHours;
+	if (hours < 10) {
+		if (hours == 0) {
+			strHours = "00";
+		}
+		else {
+			strHours = "0" + to_string(hours);
+		}
+	}
+	else {
+		strHours = to_string(hours);
+	}
+
+	string strMinutes;
+	if (minutes < 10) {
+		if (minutes == 0) {
+			strMinutes = "00";
+		}
+		else {
+			strMinutes = "0" + to_string(minutes);
+		}
+	}
+	else {
+		strMinutes = to_string(minutes);
+	}
+
+	if (delimit) {
+		return strHours + ":" + strMinutes;
+	}
+	else {
+		return strHours + strMinutes;
+	}
+}
+
+int Utils::GetDistanceBetweenPoints(POINT p1, POINT p2) {
+	// Get distance
+	return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+POINT Utils::GetMidPoint(POINT p1, POINT p2) {
+	// Return midpoint
+	return { (p1.x + p2.x) / 2, (p1.y + p2.y) / 2 };
+}
+
+int Utils::GetTimeBetweenPoints(int distanceNM, int speedGS) {
+	// Get time in minutes
+	double temp = (float)distanceNM / (float)speedGS;
+	return ((float)distanceNM / speedGS) * 60;
+}
--- VatsimNAAATS/Utils.h
+++ VatsimNAAATS/Utils.h
@@ -25,84 +25,14 @@ class Utils {
 		// TODO: Cleanup into cpp file
 
 		// Parse zulu time
-		static string ParseZuluTime(bool delimit, CFlightPlan* fp = nullptr, int ep = -1) {
-			time_t now = time(0);
-			tm* zuluTime = gmtime(&now);
-			int deltaMinutes = 0;
-			if (ep != -1) {
-				deltaMinutes = fp->GetExtractedRoute().GetPointDistanceInMinutes(ep);
-			}
-			int hours = zuluTime->tm_hour;
-			int minutes = zuluTime->tm_min + deltaMinutes;
-
-			if (minutes >= 60) {
-				// Get minutes
-				int minRemainder = minutes % 60;
-
-				// Get number of hours
-				hours += (minutes - minRemainder) / 60;
-
-				// Reassign number of minutes
-				minutes = minRemainder;
-			}
-
-			// Check if over 24 hours
-			if (hours >= 24) {
-				hours = hours - 24;
-			}
-
-
-			// Pad for zeros
-			string strHours;
-			if (hours < 10) {
-				if (hours == 0) {
-					strHours = "00";
-				}
-				else {
-					strHours = "0" + to_string(hours);
-				}
-			}
-			else {
-				strHours = to_string(hours);
-			}
-
-			string strMinutes;
-			if (minutes < 10) {
-				if (minutes == 0) {
-					strMinutes = "00";
-				}
-				else {
-					strMinutes = "0" + to_string(minutes);
-				}
-			}
-			else {
-				strMinutes = to_string(minutes);
-			}
-
-			if (delimit) {
-				return strHours + ":" + strMinutes;
-			}
-			else {
-				return strHours + strMinutes;
-			}
-		};
+		static string ParseZuluTime(bool delimit, CFlightPlan* fp = nullptr, int ep = -1);
 
 		// Distance between two points in screen coordinates
-		static int GetDistanceBetweenPoints(POINT p1, POINT p2) {
-			// Get distance
-			return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
-		}
+		static int GetDistanceBetweenPoints(POINT p1, POINT p2);
 
 		// Midpoint
-		static POINT GetMidPoint(POINT p1, POINT p2) {
-			// Return midpoint
-			return { (p1.x + p2.x) / 2, (p1.y + p2.y) / 2 };
-		}
+		static POINT GetMidPoint(POINT p1, POINT p2);
 
 		// Time (minutes) based on distance and speed
-		static int GetTimeBetweenPoints(int distanceNM,  int speedGS) {
-			// Get time in minutes
-			double temp = (float)distanceNM / (float)speedGS;
-			return ((float)distanceNM / speedGS) * 60;
-		}
+		static int GetTimeBetweenPoints(int distanceNM, int speedGS);
 };
