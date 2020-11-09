503eb01a88b0518717d4f58abc4af1ce062842c7
Ben <general@simplexwebsites.com>
2020-11-09 00:26:33 -0500

    ApiGetFlightData & ApiGetMessages are done.

---
+++ VatsimNAAATS/ApiSettings.example.h
@@ -0,0 +1,13 @@
+#ifndef API_SETTINGS_H
+#define API_SETTINGS_H
+
+#include <string>
+#pragma once
+//api info
+namespace ApiSettings
+{
+	const std::string apiUrl = "",
+		apiKey = "";
+}
+
+#endif
